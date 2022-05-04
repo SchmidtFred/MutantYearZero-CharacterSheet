7627e17bc1317a8bbe9920ad02a4a853f12b81fd
SchmidtFred <fschmidt@live.com>
2022-05-04 15:16:37 -0500

    (+)nonlinear stepper

--- client/src/Components/Create/CreateCharacter.js
+++ client/src/Components/Create/CreateCharacter.js
@@ -1,93 +1,127 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Stepper, StepLabel, Step, Button, Typography } from '@mui/material';
+import { Box, Stepper, StepButton, Step, Button, Typography } from '@mui/material';
 
 const steps = ["Role", "Character Details", "Stats", "Talent and Mutation", "Relationships", "Gear", "Den"];
 
 export default function CreateCharacter() {
     const [activeStep, setActiveStep] = useState(0);
-    const [skipped, setSkipped] = useState(new Set());
+    const [completed, setCompleted] = useState({});
+    const [roles, setRoles] = useState([]);
+    const [talents, setTalents] = useState([]);
+    const [skills, setSkills] = useState([]);
+    const [role, setRole] = useState({});
+    //is only set and fetched once a role is decided on
+    const [specialistSkill, setSpecialistSkill] = useState({});
+    //characters only start with 2 mutations at most, given at random.
+    //Randomization will occur at backend to prevent cheating
+    const [mutation, setMutation] = useState({});
+    const [secondMutation, setSecondMutation] = useState({});
+    
 
-    const isStepSkipped = (step) => {
-        return skipped.has(step);
+
+    //#region Stepper Logic
+    const totalSteps = () => {
+        return steps.length;
+    };
+
+    const completedSteps = () => {
+        return Object.keys(completed).length;
+    };
+
+    const isLastStep = () => {
+        return activeStep === totalSteps() - 1;
+    };
+
+    const allStepsCompleted = () => {
+        return completedSteps() === totalSteps();
     };
 
     const handleNext = () => {
-        let newSkipped = skipped;
-        if (isStepSkipped(activeStep)) {
-            newSkipped = new Set(newSkipped.values());
-            newSkipped.delete(activeStep);
-        }
-
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        setSkipped(newSkipped);
+        const newActiveStep = 
+            isLastStep() && !allStepsCompleted() ?
+            //If it is the last stop, but not all steps have been completed,
+            // find the first step that has been completed
+            steps.findIndex((step, i) => !(i in completed))
+            : activeStep + 1;
+        setActiveStep(newActiveStep);
     };
 
     const handleBack = () => {
         setActiveStep((prevActiveStep) => prevActiveStep - 1);
     };
 
-    const handleSkip = () => {
-
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        setSkipped((prevSkipped) => {
-            const newSkipped = new Set(prevSkipped.values());
-            newSkipped.add(activeStep);
-            return newSkipped;
-        });
+    const handleStep = (step) => () => {
+        setActiveStep(step);
     };
+
+    const handleComplete = () => {
+        const newCompleted = completed;
+        newCompleted[activeStep] = true;
+        setCompleted(newCompleted);
+        handleNext();
+    }
 
     const handleReset = () => {
         setActiveStep(0);
+        setCompleted({});
     }
+    //#endregion
 
     return (
         <Box sx={{ width: '100%' }} mt={3}>
-            <Stepper activeStep={activeStep}>
-                {steps.map((label, index) => {
-                    //set up props for the step
-                    const stepProps = {};
-                    const labelProps = {};
-                    if (isStepSkipped(index)) {
-                        stepProps.completed = false;
-                    }
-                    //return the step component
-                    return (
-                        <Step key={label} {...stepProps}>
-                            <StepLabel {...labelProps}>{label}</StepLabel>
+            <Stepper nonLinear activeStep={activeStep}>
+                {steps.map((label, index) => (
+                        <Step key={label} completed={completed[index]}>
+                            <StepButton color="inherit" onClick={handleStep(index)}>
+                                {label}
+                            </StepButton>
                         </Step>
                     )
-                })}
+                )}
             </Stepper>
-            {activeStep === steps.length ? (
-                <>
-                    <Typography sx={{ mt: 2, mb: 1}}>
-                        Finished Character Options to save character will go here.
-                    </Typography>
-                    <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                        <Box sx={{ flex: "1 1 auto"}} />
-                        <Button onClick={handleReset}>Reset (Will eventually save)</Button>
-                    </Box>
-                </>
-            ) : (
-                <>
-                    <Typography sx={{ mt: 2, mb: 1 }}>This is where we will render our form component in a Box component</Typography>
-                    <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2}}>
-                        <Button
-                            color="inherit"
-                            disabled={activeStep === 0}
-                            onClick={handleBack}
-                            sx={{ mr: 1}}
-                        >
-                            Back
-                        </Button>
-                        <Box sx={{ flex: "1 1 auto" }} />
+            <div>
+                {allStepsCompleted() ? (
+                    <>
+                        <Typography sx={{ mt: 2, mb: 1}}>
+                            Finished Character Options to save character will go here.
+                        </Typography>
+                        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                            <Box sx={{ flex: "1 1 auto"}} />
+                            <Button onClick={handleReset}>Reset (Will eventually save)</Button>
+                        </Box>
+                    </>
+                ) : (
+                    <>
+                        <Typography sx={{ mt: 2, mb: 1 }}>This is where we will render our form component in a Box component</Typography>
+                        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2}}>
+                            <Button
+                                color="inherit"
+                                disabled={activeStep === 0}
+                                onClick={handleBack}
+                                sx={{ mr: 1}}
+                            >
+                                Back
+                            </Button>
+                            <Box sx={{ flex: "1 1 auto" }} />
 
-                        <Button onClick={handleNext}>
-                            {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                        </Button>
-                    </Box>
-                </>
-            )}
+                            <Button onClick={handleNext}>
+                                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                            </Button>
+                            {activeStep !== steps.length && (completed[activeStep] ? (
+                                <Typography variant="caption" sx={{ display: 'inline-block' }}>
+                                    {steps[activeStep]} completed
+                                </Typography>
+                            ): (
+                                <Button onClick={handleComplete}>
+                                    {completedSteps() === totalSteps() - 1
+                                    ? 'Finish Incomplete Steps'
+                                    : 'Complete Step'}
+                                </Button>
+                            ))}
+                        </Box>
+                    </>
+                )}
+            </div>
         </Box>
-    )
+    );
 }
